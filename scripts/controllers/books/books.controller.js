ab908982849c6341b6aee656601ff6966d19f377
Jaime Neves <jaime.neves@apassos.com.br>
2018-09-18 17:59:09 -0400

    add books controller

---
+++ scripts/controllers/books/books.controller.js
@@ -0,0 +1,19 @@
+(function () {
+  'use strict'
+
+  angular
+    .module('app')
+    .controller('BooksController', BooksController)
+
+  function BooksController () {
+    var vm = this
+
+    vm.listAll = function () {
+
+    }
+
+    vm.listById = function (id) {
+
+    }
+  }
+})()
